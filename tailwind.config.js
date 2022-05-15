8bbf41d8fadc1f7045ba70decb36aaf2c2302a6e
fadhilsaheer <fadhilsaheer@gmail.com>
2022-05-15 09:21:18 +0530

    theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        app: {
+          dark1: '#1F1D2B',
+          dark2: '#272636',
+          light1: '#F9F9F9',
+          light2: '#D1D1D1',
+          accent: '#6F6FC8',
+        },
+      },
+    },
   },
   plugins: [],
 };
